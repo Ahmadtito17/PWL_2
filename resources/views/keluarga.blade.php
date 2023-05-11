f397accbef859f53206a277ef0461b3f7f19c10f
= Ahmad Tito <= ahmadtitohendrawan@gmail.com>
2023-05-11 19:40:11 +0700

    Update Blade

--- resources/views/keluarga.blade.php
+++ resources/views/keluarga.blade.php
@@ -4,7 +4,7 @@
 <div class="container-fluid">
     <div class="row mb-2">
       <div class="col-sm-6">
-        <h1>Profile</h1>
+        <h1>Keluarga</h1>
       </div>
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
@@ -22,7 +22,7 @@
   <!-- Default box -->
   <div class="card">
     <div class="card-header">
-      <h3 class="card-title">Keluarga</h3>
+      <h3 class="card-title">Data Keluarga</h3>
 
       <div class="card-tools">
         <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
@@ -55,10 +55,5 @@
         @endforeach
       </table>
     </div>
-    <!-- /.card-body -->
-    <div class="card-footer">
-      Footer
-    </div>
-    <!-- /.card-footer-->
   </div>
 @endsection
--- resources/views/mahasiswa/create_mahasiswa.blade.php
+++ resources/views/mahasiswa/create_mahasiswa.blade.php
@@ -4,7 +4,7 @@
 <div class="container-fluid">
     <div class="row mb-2">
       <div class="col-sm-6">
-        <h1>Profile</h1>
+        <h1>Tambah Data Mahasiswa</h1>
       </div>
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
@@ -99,10 +99,5 @@
         </div>
       </form>
     </div>
-    <!-- /.card-body -->
-    <div class="card-footer">
-      Footer
-    </div>
-    <!-- /.card-footer-->
   </div>
 @endsection
